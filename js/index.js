3e84677df35ddd6abefcc0b94d57a10d96a353cb
ohmgmatt <bonilla.matthew44@gmail.com>
2018-06-16 19:28:15 -0700

    Introduce the dataset and xscale for the scatter plot

--- js/index.js
+++ js/index.js
@@ -1,24 +1,38 @@
 
 // Set the height and width
 var w = 500, h = 500;
+var padding = 10;
 
-
+// Edit the dataset
 
 // Create the draw function
 function draw(dataset) {
 
+  // Clean the dataset
+  dataset.forEach(function(d) { d.HR = +d.HR;});
+  dataset.forEach(function(d) { d.height = +d.height;});
+  dataset.forEach(function(d) { d.weight = +d.weight;});
+
+  // Create the scales
+  var xScale = d3.scaleLinear()
+                .domain([d3.min(dataset, function(d){ return d.height;}),
+                          d3.max(dataset, function(d){ return d.height;})
+                        ])
+                .range([padding, w - padding]);
+
+  // Create the SVG
   var canvas = d3.select("body")
             .append("svg")
             .attr("width", w)
             .attr("height", h);
 
-  var bar = canvas.selectAll("rect")
-              .data(dataset)
-              .enter()
-                .append("rect")
-                .attr("width", function(d) { return +d.HR; })
-                .attr("height", 1)
-                .attr("y", function(d, i) { return i; });
-
+  //Create the points
+  var point = canvas.selectAll("circle")
+            .data(dataset)
+            .enter()
+              .append("circle")
+              .attr("cx", function(d) { return xScale(d.height); })
+              .attr("cy", function(d) { return d.weight; })
+              .attr("r", 2);
 
 }
