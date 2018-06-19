c66b38c671678d4a8804e188e9fd4edd2fb73c0e
ohmgmatt <bonilla.matthew44@gmail.com>
2018-06-18 21:24:47 -0700

    Exploring aggregate methods for dimple charts

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
 function draw(data) {
 
-  data.forEach(function(d) { d.HR = +d.HR;});
-  data.forEach(function(d) { d.avg = +d.avg;});
-  data.forEach(function(d) { d.height = +d.height;});
-  data.forEach(function(d) { d.weight = +d.weight;});
+  //data.forEach(function(d) { d.HR = +d.HR;});
+  //data.forEach(function(d) { d.avg = +d.avg;});
+  //data.forEach(function(d) { d.height = +d.height;});
+  //data.forEach(function(d) { d.weight = +d.weight;});
 
+  plot1(data);
+  plot2(data);
+
+  //d3.select("body").append("p").text("THIS IS THE END");
+
+}
+function plot1(data) {
   var padding = 75;
-  var w = 1000 - padding,
-      h = 400 - padding;
+  var w = 1400 - padding,
+      h = 600 - padding;
 
   var svg1 = d3.select("body")
               .append("svg")
@@ -17,10 +24,18 @@ function draw(data) {
                 .attr("class", "chart");
 
   var myChart1 = new dimple.chart(svg1, data);
-  myChart1.addCategoryAxis("x", "handedness");
+  myChart1.addCategoryAxis("x", "height");
   myChart1.addMeasureAxis("y", "HR");
   myChart1.addSeries(null, dimple.plot.bar)
+    .aggregate = dimple.aggregateMethod.avg;
+  myChart1.setBounds(padding,padding,w - padding,h - padding);
   myChart1.draw();
+
+}
+function plot2(data) {
+  var padding = 75;
+  var w = 1400 - padding,
+      h = 600 - padding;
 
   var svg2 = d3.select("body")
             .append("svg")
@@ -33,6 +48,7 @@ function draw(data) {
   myChart2.addCategoryAxis("x", "handedness");
   myChart2.addMeasureAxis("y", "avg");
   myChart2.addSeries(null, dimple.plot.bar)
+    .aggregate = dimple.aggregateMethod.avg;
+  myChart2.setBounds(padding,padding,w - padding,h - padding);
   myChart2.draw();
-
 }
