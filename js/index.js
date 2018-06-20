f9592774a4f512c58f430131598577765bffd675
ohmgmatt <bonilla.matthew44@gmail.com>
2018-06-19 19:10:23 -0700

    Set up HTML to introduce paragraphs. Fixed JS to handle new HTML code

--- js/index.js
+++ js/index.js
@@ -1,22 +1,17 @@
 function draw(data) {
 
-  //data.forEach(function(d) { d.HR = +d.HR;});
-  //data.forEach(function(d) { d.avg = +d.avg;});
-  //data.forEach(function(d) { d.height = +d.height;});
-  //data.forEach(function(d) { d.weight = +d.weight;});
-
   plot1(data);
   plot2(data);
 
-  //d3.select("body").append("p").text("THIS IS THE END");
-
 }
-function plot1(data) {
-  var padding = 75;
-  var w = 1400 - padding,
-      h = 600 - padding;
 
-  var svg1 = d3.select("body")
+
+function plot1(data) {
+  var padding = 50;
+  var w = 750 - padding,
+      h = 450 - padding;
+
+  var svg1 = d3.select("body").select("#chart1")
               .append("svg")
               .attr("width", w + padding)
               .attr("height", h + padding)
@@ -25,7 +20,7 @@ function plot1(data) {
 
   var myChart1 = new dimple.chart(svg1, data);
   myChart1.addCategoryAxis("x", "height");
-  myChart1.addMeasureAxis("y", "HR");
+  myChart1.addMeasureAxis("y", "avg");
   myChart1.addSeries(null, dimple.plot.bar)
     .aggregate = dimple.aggregateMethod.avg;
   myChart1.setBounds(padding,padding,w - padding,h - padding);
@@ -33,11 +28,11 @@ function plot1(data) {
 
 }
 function plot2(data) {
-  var padding = 75;
-  var w = 1400 - padding,
-      h = 600 - padding;
+  var padding = 50;
+  var w = 750 - padding,
+      h = 450 - padding;
 
-  var svg2 = d3.select("body")
+  var svg2 = d3.select("body").select("#chart2")
             .append("svg")
               .attr("width", w + padding)
               .attr("height", h + padding)
@@ -48,7 +43,7 @@ function plot2(data) {
   myChart2.addCategoryAxis("x", "handedness");
   myChart2.addMeasureAxis("y", "avg");
   myChart2.addSeries(null, dimple.plot.bar)
-    .aggregate = dimple.aggregateMethod.avg;
+    .aggregate = dimple.aggregateMethod.count;
   myChart2.setBounds(padding,padding,w - padding,h - padding);
   myChart2.draw();
 }
