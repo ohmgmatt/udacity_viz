8bbb416bd77da19106817deeae482c07ecd133e4
ohmgmatt <bonilla.matthew44@gmail.com>
2018-06-20 08:00:51 -0700

    Commenting code

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
+// This creates the larger draw function and is what is called by the
+// main HTML code.
 function draw(data) {
 
   plot1(data);
   plot2(data);
-
 }
 
-
+// Function to draw the first charts where we compare how height
+// and weight affect performance.
 function plot1(data) {
+
   var padding = 50;
   var w = 750 - padding,
       h = 450 - padding;
@@ -27,6 +30,9 @@ function plot1(data) {
   myChart1.draw();
 
 }
+
+// Function to draw the second chart where we compare how handedness
+// affect performance by seeing their average for both HR and batting avg.
 function plot2(data) {
   var padding = 50;
   var w = 750 - padding,
@@ -41,9 +47,10 @@ function plot2(data) {
 
   var myChart2 = new dimple.chart(svg2,data);
   myChart2.addCategoryAxis("x", "handedness");
-  myChart2.addMeasureAxis("y", "avg");
+  myChart2.addMeasureAxis("y", "HR");
   myChart2.addSeries(null, dimple.plot.bar)
-    .aggregate = dimple.aggregateMethod.count;
+    .aggregate = dimple.aggregateMethod.avg;
   myChart2.setBounds(padding,padding,w - padding,h - padding);
   myChart2.draw();
+
 }
