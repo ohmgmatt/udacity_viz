a3b5cdf2b70c24f722c5418bbd5ae6a7c011c311
ohmgmatt <bonilla.matthew44@gmail.com>
2018-06-24 20:55:17 -0700

    Fix axis titles and graph shape

--- js/index.js
+++ js/index.js
@@ -1,16 +1,14 @@
 //Establishing Global Variables
 
-var padding = 50;
-  var w = 850 - padding,
+var padding = 70;
+  var w = 900 - padding,
       h = 500 - padding;
 
 // This creates the larger draw function and is what is called by the
 // main HTML code.
 function draw(data) {
-
   plot1(data);
   plot2(data);
-
 }
 
 // Function to draw the first charts where we compare how height
@@ -27,15 +25,20 @@ function plot1(data) {
 
   var myChart1 = new dimple.chart(svg1, data);
 
-  function chart1(data, category, measure) {
+  function chart1(data, category, measure, xtitle) {
     var xAxis = myChart1.addCategoryAxis("x",category);
       xAxis.addOrderRule(category);
+      xAxis.fontSize = "12px";
+      xAxis.title = xtitle;
     var yAxis = myChart1.addMeasureAxis("y", measure);
+      yAxis.showGridlines = true;
+      yAxis.tickFormat = ',.3f';
     myChart1.addSeries(null, dimple.plot.bar)
       .aggregate = dimple.aggregateMethod.avg;
     myChart1.setBounds(padding,padding, w - padding, h - padding);
     myChart1.draw(750);
   }
+
   var physical = ['Height - HR', 'Height - AVG', 'Weight - HR', 'Weight - AVG']
 
   d3.select("#set1").selectAll("button")
@@ -46,25 +49,33 @@ function plot1(data) {
       .attr("class","buttons1")
     .on("click",function(d) {
       if (d == 'Height - HR') {
+
             myChart1.svg.selectAll('*').remove();
             myChart1 = new dimple.chart(svg1, data);
-            chart1(data, "height", "HR");
+            chart1(data, "height", "HR", "Height (inches)");
+
           } else if (d == 'Height - AVG') {
+
             myChart1.svg.selectAll("*").remove();
             myChart1 = new dimple.chart(svg1, data);
-            chart1(data, "height", "avg");
+            chart1(data, "height", "avg", "Height (inches)");
+
           } else if (d == "Weight - HR") {
+
             myChart1.svg.selectAll("*").remove();
             myChart1 = new dimple.chart(svg1, data);
-            chart1(data, "weight", "HR");
+            chart1(data, "weight", "HR", "Weight (lbs)");
+
           } else if (d == 'Weight - AVG') {
+
             myChart1.svg.selectAll("*").remove();
             myChart1 = new dimple.chart(svg1,data);
-            chart1(data, "weight", "avg");
+            chart1(data, "weight", "avg", "Weight (lbs)");
+
           }
     });
 
-  chart1(data, "height", "HR");
+  chart1(data, "height", "HR", "Height (inches)");
 }
 
 // Function to draw the second chart where we compare how handedness
@@ -81,8 +92,11 @@ function plot2(data) {
   var myChart2 = new dimple.chart(svg2,data);
 
   function chart2(data, measure, aggr) {
-    myChart2.addCategoryAxis("x", "handedness");
-    myChart2.addMeasureAxis("y", measure);
+    var xAxis = myChart2.addCategoryAxis("x", "handedness");
+      xAxis.fontSize = "12px";
+      xAxis.title = "Handedness";
+    var yAxis = myChart2.addMeasureAxis("y", measure);
+      yAxis.tickFormat = ',.3f';
     myChart2.addSeries(null, dimple.plot.bar)
       .aggregate = aggr;
     myChart2.setBounds(padding,padding,w - padding,h - padding);
@@ -99,17 +113,23 @@ function plot2(data) {
       .attr("class","buttons2")
     .on("click", function(d) {
       if (d == "Average of Home Runs") {
+
         myChart2.svg.selectAll("*").remove();
         myChart2 = new dimple.chart(svg2, data);
         chart2(data, "HR", dimple.aggregateMethod.avg);
+
       } else if (d == "Average of Batting Averages") {
+
         myChart2.svg.selectAll("*").remove();
         myChart2 = new dimple.chart(svg2, data);
         chart2(data, "avg", dimple.aggregateMethod.avg);
+
       } else if ( d == "Amount of Players") {
+
         myChart2.svg.selectAll("*").remove();
         myChart2 = new dimple.chart(svg2, data);
         chart2(data, "HR", dimple.aggregateMethod.count);
+
       }
     });
   chart2(data, "HR", dimple.aggregateMethod.count);
